f619df82dc49d6f540ad95d1414310a8a6d68d92
15810111831 <15810111831@163.com>
2019-05-18 22:07:59 +0800

    更新

--- dailyfresh/common/base_admin_site.py
+++ dailyfresh/common/base_admin_site.py
@@ -4,8 +4,8 @@ from django.contrib.admin.sites import AdminSite
 
 
 class MyAdminSite(AdminSite):
-    site_header = '天天生鲜管理平台'
-    site_title = '天天生鲜后台管理系统'
+    site_header = '水果销售管理平台'
+    site_title = '水果销售后台管理系统'
 
 
 admin_site = MyAdminSite()
